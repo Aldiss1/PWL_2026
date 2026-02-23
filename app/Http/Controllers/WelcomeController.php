09a13b124a8203985f52d40520b890936a693d84
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-02-23 22:29:31 +0700

    JS2_View_Percobaan4

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,6 +10,8 @@ public function hello(){
 return('Hello World');
 }
 public function greeting(){
-return view('blog.hello', ['name' => 'Aldi Surya Saputra']);
+return view('blog.hello')
+->with('name','Aldi Surya Saputra')
+->with('occupation','Mahasiswa');
 }
 }
--- resources/views/blog/hello.blade.php
+++ resources/views/blog/hello.blade.php
@@ -1,5 +1,6 @@
 <html>
 <body>
     <h1>Hello, {{ $name }}</h1>
+    <h1>Anda adalah {{ $occupation }}</h1>
 </body>
 </html>
