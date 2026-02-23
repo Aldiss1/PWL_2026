ba4ea171ce9346dd26d67a1f05165fefe4ee3453
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-02-23 21:37:20 +0700

    JS2_Routing_Percobaan2

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,15 @@ Route::get('/nim-nama', function () {
     return 'NIM : 244107020184 Nama : Aldi Surya Sap';
 });
 
+Route::get('/user/{name}', function ($name) {
+return 'Nama saya '.$name;
+});
 
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+return 'Halaman Artikel dengan ID '.$id;
+});
