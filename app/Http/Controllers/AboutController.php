01003c245f30c442870ae9e6bd4592da20fb4cac
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-02-23 21:51:05 +0700

    JS2_Controller_Percobaan1

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index()
+    {
+        return 'Nama: Aldi Surya Saputra | NIM: 244107020184';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+// Single Action Controllers (tanpa __invoke)
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
 
+// WelcomeController
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+// Other Routes
 Route::get('/world', function () {
-return 'World';
+    return 'World';
 });
 Route::get('/welcome', function () {
     return 'Selamat datang di website saya';
 });
-
 Route::get('/nim-nama', function () {
-    return 'NIM : 244107020184 Nama : Aldi Surya Sap';
+    return 'NIM : 244107020184 Nama : Aldi Surya Saputra';
 });
-
 Route::get('/user/{name}', function ($name) {
-return 'Nama saya '.$name;
+    return 'Nama saya ' . $name;
 });
-
-Route::get('/posts/{post}/comments/{comment}', function
-($postId, $commentId) {
-return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' Komentar ke-: ' . $commentId;
 });
-
-Route::get('/articles/{id}', function ($id) {
-return 'Halaman Artikel dengan ID '.$id;
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
 });
-
-
-Route::get('/user/{name?}', function ($name=null) {
-return 'Nama saya '.$name;
-});
-
-Route::get('/user/{name?}', function ($name='John') {
-return 'Nama saya '.$name;
-});
-
