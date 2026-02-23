24b3256d75734a795c7cd1cdef5c9f79f03d20f1
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-02-23 21:33:37 +0700

    JS2_Routing_Percobaan1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+return 'World';
+});
+Route::get('/welcome', function () {
+    return 'Selamat datang di website saya';
+});
+
+Route::get('/nim-nama', function () {
+    return 'NIM : 244107020184 Nama : Aldi Surya Sap';
+});
+
+
